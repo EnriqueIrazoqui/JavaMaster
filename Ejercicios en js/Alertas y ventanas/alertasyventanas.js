67e7f5d3708e3e3cd3abb581efb77327fb7a0b62
Enrique Irazoqui <za19012238@zapopan.tecmm.edu.mx>
2022-10-08 18:03:31 -0500

    ventanas y alertas

---
+++ Ejercicios en js/Alertas y ventanas/alertasyventanas.js
@@ -0,0 +1,14 @@
+'use estrict'
+// alerta
+alert("Esta es mi alerta");
+alert("Esta es mi texto");
+
+//alertas de confirmacion
+var resultado = confirm("Estas seguro de querer continuar?");
+console.log(resultado);
+
+//ventana de ingreso de informacion
+var edad = prompt("Que edad tienes?",18);
+console.log(typeof edad);
+
+
