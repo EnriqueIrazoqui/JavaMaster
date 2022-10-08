f64b7c6a9b71820d77f8d125e4363fe1e80e8697
Enrique Irazoqui <za19012238@zapopan.tecmm.edu.mx>
2022-10-08 01:50:10 -0500

    operadores y tipos de datos

---
+++ Ejercicios en js/Tipos de datos y operadores/tipos_de_datos_operadores.js
@@ -0,0 +1,27 @@
+'use strict'
+//alert("hola");
+//Operadores
+var numero = 10;
+var numero2 = 12;
+var operacion = numero + numero2;
+var operacion = numero - numero2;
+var operacion = numero * numero2;
+var operacion = numero / numero2;
+var operacion = numero % numero2;
+
+alert("Resultado: "+operacion);
+
+//Tipos de datos 
+var numero_entero = 44;
+var cadena = 'Hola "que" tal';
+var verdadero_falso = true;
+console.log(verdadero_falso);
+
+var numero_falso = "33";
+Number();
+console.log(Number(numero_falso)+6);
+console.log(parseInt(numero_falso)+6);
+console.log(parseFloat(numero_falso)+6);
+
+console.log(typeof numero);
+console.log(typeof numero2);
